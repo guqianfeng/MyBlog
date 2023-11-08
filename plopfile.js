3ad582925f32b938e825a0c70b91508b33032225
guqianfeng <fred_gqf0221@163.com>
2023-11-08 09:47:38 +0800

    plop modify route

--- plopfile.js
+++ plopfile.js
@@ -7,10 +7,18 @@ module.exports = function (plop) {
             name: 'name',
             message: 'cocos base 2d title name please'
         }],
-        actions: [{
-            type: 'add',
-            path: 'docs/cocos/base/2d/{{name}}/index.md',
-            templateFile: 'plop-templates/cocos/base/2d/index.md.hbs'
-        }]
+        actions: [
+            {
+                type: 'add',
+                path: 'docs/cocos/base/2d/{{name}}/index.md',
+                templateFile: 'plop-templates/cocos/base/2d/index.md.hbs'
+            },
+            {
+                type: 'modify', // 修改文件
+                path: 'docs/.vitepress/router/cocos/base.mts', // 修改文件路径
+                pattern: /(\/\/ -- append route here --)/gi, // 正则找到标识位置(在文件哪里修改)
+                templateFile: 'plop-templates/cocos/base/2d/route.ts.hbs' // 模板路径
+            }
+        ]
     });
 };
