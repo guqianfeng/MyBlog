1db4a5917bf36bb6a98e1e2afc81562bb9c52b54
guqianfeng <fred_gqf0221@163.com>
2023-11-16 14:44:58 +0800

    plopfile

--- plopfile.js
+++ plopfile.js
@@ -42,4 +42,48 @@ module.exports = function (plop) {
             }
         ]
     });
+
+    plop.setGenerator('3DBase', {
+        description: '请输入3D游戏相关-基础知识标题',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: '请输入3D游戏相关-基础知识标题'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'docs/cocos/3d/base/{{name}}/index.md',
+                templateFile: 'plop-templates/cocos/3d/index.md.hbs'
+            },
+            {
+                type: 'modify', // 修改文件
+                path: 'docs/.vitepress/router/cocos/3d.mts', // 修改文件路径
+                pattern: /(\/\/ -- append base route here --)/gi, // 正则找到标识位置(在文件哪里修改)
+                templateFile: 'plop-templates/cocos/3d/base.route.ts.hbs' // 模板路径
+            }
+        ]
+    });
+
+    plop.setGenerator('3DGame', {
+        description: '请输入3D游戏相关-游戏案例名',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: '请输入3D游戏相关-游戏案例名'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'docs/cocos/3d/game/案例-{{name}}/index.md',
+                templateFile: 'plop-templates/cocos/3d/index.md.hbs'
+            },
+            {
+                type: 'modify', // 修改文件
+                path: 'docs/.vitepress/router/cocos/3d.mts', // 修改文件路径
+                pattern: /(\/\/ -- append game route here --)/gi, // 正则找到标识位置(在文件哪里修改)
+                templateFile: 'plop-templates/cocos/3d/game.route.ts.hbs' // 模板路径
+            }
+        ]
+    });
 };
